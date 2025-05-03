6a42774b0c414f19975f1d2db1eef670cb5b40de
Sammyduzit <sammy.duzit999@gmail.com>
2025-05-03 21:52:35 +0200

    SOC, finished data handling and API Endpoints

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,18 +1,164 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import data_handler
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
-
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
+CORS(app)
 
 
-@app.route('/api/posts', methods=['GET'])
-def get_posts():
-    return jsonify(POSTS)
+def error_response(message, status_code, details=None):
+    """
+    Generate error response JSON.
+    :param message: Main error message.
+    :param status_code: HTTP status code.
+    :param details: Additional details about the error. Default=None
+    :return: tuple: (JSON response, status code)
+    """
+    response = {
+        "error": {
+            "message": message,
+            "status": status_code,
+            "details": details or []
+        }
+    }
+    return jsonify(response), status_code
+
+
+def success_response(data, status_code=200):
+    """
+    Generate success response JSON.
+    :param data: Data to return.
+    :param status_code: HTTP status code.
+    :return: tuple: ( JSON response, status code)
+    """
+    return jsonify(data), status_code
+
+
+@app.route("/api/posts", methods=["GET"])
+def list_posts():
+    """
+    Lists all posts, optionally sorted by given parameters.
+    :return: tuple:
+        - if successful: (sorted list as JSON, status code)
+        - if unsuccessful: (error message, status code)
+    """
+    posts = data_handler.get_all_posts()
+    sort = request.args.get("sort")
+    direction = request.args.get("direction")
+
+    if not sort:
+        return success_response(posts)
+
+    is_valid, errors = data_handler.validate_sorting(sort, direction)
+    if not is_valid:
+        return error_response("Invalid sorting parameters", 400, errors)
+
+    try:
+        sorted_posts = data_handler.sort_posts(posts, sort, direction)
+        return success_response(sorted_posts)
+    except KeyError as e:
+        return error_response(f"Missing key in post data {e}", 500)
+    except Exception as e:
+        return  error_response(f"Server error while sorting", 500, f"error: {e}")
+
+
+@app.route("/api/posts", methods=["POST"])
+def add_post():
+    """
+    Adds a new post. Uses validate_post_data function for validation and create_post for post creation.
+    :return: tuple:
+        - if successful: (added post as JSON, status code)
+        - if unsuccessful: (error message, status code)
+    """
+    if not request.is_json:
+        return error_response("Request must be JSON", 400)
+
+    data = request.get_json()
+    is_valid, errors = data_handler.validate_post_data(data)
+
+    if not is_valid:
+        return error_response("Invalid post data", 400, errors)
+
+    try:
+        new_post = data_handler.create_post(data["title"], data["content"])
+        return success_response(new_post, 201)
+    except Exception as e:
+        return error_response("Server error while adding post", 500, f"error: {e}")
+
+
+@app.route("/api/posts/<int:post_id>", methods=["DELETE"])
+def delete_post(post_id):
+    """
+    Deletes a post by ID. Checks if post was deleted.
+    :param post_id: ID of post to be deleted.
+    :return: tuple:
+        - if successful: (success message as JSON, status code)
+        - if unsuccessful: (error message, status code)
+    """
+    posts = data_handler.get_all_posts()
+    initial_number_of_posts = len(posts)
+
+    posts = [post for post in posts if post["id"] != post_id]
+
+    if len(posts) < initial_number_of_posts:
+        data_handler.save_all_posts(posts)
+        return success_response({"message": f"Post with id '{post_id}' has been deleted successfully."})
+
+    else:
+        return error_response(f"No post with id <{post_id}> found.", 404)
+
+
+
+@app.route("/api/posts/<int:post_id>", methods=["PUT"])
+def update_post(post_id):
+    """
+    Updates a post by ID.
+    :param post_id: ID of post to be updated.
+    :return: tuple:
+        - if successful: (updated post as JSON, status code)
+        - if unsuccessful: (error message, status code)
+    """
+    if not request.is_json:
+        return error_response("Request must be JSON", 400)
+
+    posts = data_handler.get_all_posts()
+    post = next((post for post in posts if post["id"] == post_id), None)
+
+    if not post:
+        return error_response("Post not found.", 404)
+
+    data_for_update = request.get_json()
+
+    is_successful, error = data_handler.updating_post(post, data_for_update)
+
+    if not is_successful:
+        return error_response("Invalid update data", 400, error)
+
+    data_handler.save_all_posts(posts)
+    return success_response(post)
+
+
+
+@app.route("/api/posts/search", methods=["GET"])
+def search_post():
+    """
+    Searches for posts by title or content.
+    :return: tuple:
+        - if successful: (list of matching posts as JSON, status code)
+        - if unsuccessful: (error message, status code)
+    """
+    posts = data_handler.get_all_posts()
+    title = request.args.get("title")
+    content = request.args.get("content")
+
+    if not (title or content):
+        return error_response("Search must have at least one parameter", 400)
+
+    match_list = [post for post in posts
+                  if (not title or title.lower() in post["title"].lower())
+                  and (not content or content.lower() in post["content"].lower())]
+
+    return success_response(match_list)
 
 
 if __name__ == '__main__':
---
+++ backend/data_handler.py
@@ -0,0 +1,159 @@
+"""
+Data handling for Masterblog API
+"""
+
+import json
+
+BLOGPOSTS = "data/posts.json"
+
+
+def load_json(filepath):
+    """
+    Load blog posts from JSON file.
+    :param filepath: Path to the JSON file.
+    :return: Parsed JSON data as a Python object, or empty list if an error occurs.
+    """
+    try:
+        with open(filepath, "r") as handle:
+            return json.load(handle)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def save_json(filepath, data):
+    """
+    Save blog posts to JSON file.
+    :param filepath: Path to the JSON file.
+    :param data: Data to be saved.
+    :return: None
+    """
+    with open(filepath, "w") as handle:
+        json.dump(data, handle, indent=4)
+
+
+def get_all_posts():
+    """
+    Retrieve all posts from json file.
+    :return: List of all posts (list of dicts), or empty list if no posts are found.
+    """
+    return load_json(BLOGPOSTS)
+
+
+def save_all_posts(posts):
+    """
+    Saves a list of posts to json file.
+    :param posts: List of posts to save.
+    :return: None
+    """
+    save_json(BLOGPOSTS, posts)
+
+
+def get_all_fields():
+    """
+    Retrieves all unique fields in the posts data, excluding 'id'.
+    :return: Set of field names excluding 'id'.
+    """
+    posts = get_all_posts()
+    return {key for post in posts for key in post if key != "id"}
+
+
+def validate_sorting(sort, direction):
+    """
+    Validate sorting parameters for POST queries.
+    :param sort: Parameter to sort by.
+    :param direction: Sorting direction (ascending 'asc' or descending 'desc')
+    :return: tuple: (is_valid: bool, errors: list)
+    """
+    errors = []
+    valid_sorts = get_all_fields()
+    valid_directions = {"asc", "desc"}
+
+    if direction and not sort:
+        errors.append("Direction parameter requires sort parameter.")
+
+    if sort:
+        if sort not in valid_sorts:
+            errors.append(f"Invalid sort parameter: {sort}. "
+                                     f"Valid options are '{', '.join(valid_sorts)}'")
+        if direction and direction not in valid_directions:
+            errors.append(f"Invalid direction parameter: {direction}. "
+                                     f"Valid options are '{', '.join(valid_directions)}'")
+
+    return len(errors) == 0, errors
+
+
+def sort_posts(posts, sort_key, direction):
+    """
+    Sorts a list of posts by given sort key and direction.
+    :param posts: List of posts to sort.
+    :param sort_key: Key to sort by.
+    :param direction: Sorting direction (ascending 'asc' or descending 'desc')
+    :return: Sorted list of posts.
+    """
+    reverse = (direction == "desc")
+    return sorted(posts, key=lambda x: x[sort_key], reverse=reverse)
+
+
+def validate_post_data(data):
+    """
+    Validates post data. Checks for required fields and data type.
+    :param data: Post data to validate.
+    :return: tuple: (is_valid: bool, errors: list)
+    """
+    errors = []
+    required_fields = get_all_fields()
+    for field in required_fields:
+        if field not in data:
+            errors.append(f"Missing required field: {field}")
+            continue
+
+        if not isinstance(data[field], str):
+            errors.append(f"{field} must be a string.")
+
+        elif not data[field].strip():
+            errors.append(f"{field} can not be empty.")
+
+    return len(errors) == 0, errors
+
+
+def create_post(title, content):
+    """
+    Creates a new post and assigns unique post id.
+    :param title: Title of the new post.
+    :param content: Content of the new post.
+    :return: New post as dict.
+    """
+    posts = get_all_posts()
+
+    new_post_id = 1
+    if posts:
+        new_post_id = max(post["id"] for post in posts) + 1
+
+    new_post = {
+        "id": new_post_id,
+        "title": title,
+        "content": content
+    }
+
+    posts.append(new_post)
+    save_all_posts(posts)
+
+    return new_post
+
+
+def updating_post(post, data_for_update):
+    """
+    Updates a post with the provided data to update.
+    :param post: Post to update.
+    :param data_for_update: Data tu be updated.
+    :return: tuple:
+        - if successful: (is_successful: bool, error: str)
+        - if unsuccessful: (error message, status code)
+    """
+    allowed_fields = get_all_fields()
+    for field in data_for_update:
+        if field in allowed_fields:
+            post[field] = data_for_update[field]
+        else:
+            return False, f"'{field}' is no allowed Field."
+    return True, None
